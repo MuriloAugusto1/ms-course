3e98e7796487ed2a23535eb3e9beeedc3c7f0736
Murilo <muriloalf@gmail.com>
2024-02-13 20:55:42 -0300

    implementing hr-payroll mock

---
+++ hr-payroll/hr-payroll/src/main/java/io/github/muriloaugusto1/hrpayroll/resources/PaymentResource.java
@@ -0,0 +1,25 @@
+package io.github.muriloaugusto1.hrpayroll.resources;
+
+import io.github.muriloaugusto1.hrpayroll.entities.Payment;
+import io.github.muriloaugusto1.hrpayroll.services.PaymentService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/payments")
+public class PaymentResource {
+    private final PaymentService paymentService;
+
+    public PaymentResource(PaymentService paymentService) {
+        this.paymentService = paymentService;
+    }
+
+    @GetMapping("/{workerId}/days/{days}")
+    public ResponseEntity<Payment> getPayment(@PathVariable Long workerId, @PathVariable Integer days) {
+        Payment payment = paymentService.getPayment(workerId, days);
+        return ResponseEntity.ok(payment);
+    }
+}
