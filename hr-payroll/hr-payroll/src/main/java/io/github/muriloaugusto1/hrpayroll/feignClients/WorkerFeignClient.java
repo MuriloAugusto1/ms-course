e1682258529f6657e765054274b65057224981da
Murilo <muriloalf@gmail.com>
2024-02-13 21:29:45 -0300

    creating feign client

---
+++ hr-payroll/hr-payroll/src/main/java/io/github/muriloaugusto1/hrpayroll/feignClients/WorkerFeignClient.java
@@ -0,0 +1,17 @@
+package io.github.muriloaugusto1.hrpayroll.feignClients;
+
+import io.github.muriloaugusto1.hrpayroll.entities.Worker;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@Component
+@FeignClient(name = "hr-worker", url = "localhost:8001", path = "/workers")
+public interface WorkerFeignClient {
+
+    @GetMapping("/{id}")
+    ResponseEntity<Worker> findById(@PathVariable Long id);
+}
--- hr-payroll/hr-payroll/src/main/java/io/github/muriloaugusto1/hrpayroll/services/PaymentService.java
+++ hr-payroll/hr-payroll/src/main/java/io/github/muriloaugusto1/hrpayroll/services/PaymentService.java
@@ -1,12 +1,26 @@
 package io.github.muriloaugusto1.hrpayroll.services;
 
 import io.github.muriloaugusto1.hrpayroll.entities.Payment;
+import io.github.muriloaugusto1.hrpayroll.entities.Worker;
+import io.github.muriloaugusto1.hrpayroll.feignClients.WorkerFeignClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class PaymentService {
+    private final WorkerFeignClient workerFeignClient;
+
+    public PaymentService(WorkerFeignClient workerFeignClient) {
+        this.workerFeignClient = workerFeignClient;
+    }
 
     public Payment getPayment(long workerId, int days) {
-        return new Payment("Bob", 200.0, days);
+
+        Worker worker = workerFeignClient.findById(workerId).getBody();
+        return new Payment(worker.getName(), worker.getDailyIncome(), days);
     }
 }
